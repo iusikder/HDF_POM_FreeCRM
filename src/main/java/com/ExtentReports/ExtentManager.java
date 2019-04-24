094b64df565f356ffc7b2c144c21fdf9bc1eff2b
iusikder <iusikder@yahoo.com>
2019-04-24 04:28:29 -0400

    Updated Path

--- src/main/java/com/ExtentReports/ExtentManager.java
+++ src/main/java/com/ExtentReports/ExtentManager.java
@@ -1,5 +1,6 @@
 package com.ExtentReports;
 
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 import com.relevantcodes.extentreports.ExtentReports;	 
 //OB: ExtentReports extent instance created here. That instance can be reachable by getReporter() method.
 	
@@ -13,7 +14,7 @@ public class ExtentManager {
         if(extent == null){
             //Set HTML reporting file location
             String workingDir = System.getProperty("user.dir");
-            extent = new ExtentReports(workingDir+"\\ExtentReports\\ExtentReportResults.html");
+            extent = new ExtentReports(workingDir+"./ExtentReports/ExtentReportResults.html");
             //extent = new ExtentReports(workingDir+"\\test-output\\Extent.FreeCRM.html", false);
         }
         return extent;
